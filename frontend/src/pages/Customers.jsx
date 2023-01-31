21907c87dafd5aff33b8311fa38758b6c99868d3
FidaHussain87 <36170696+FidaHussain87@users.noreply.github.com>
2023-01-31 04:26:24 +0100

    pages,app components done,

--- frontend/src/pages/Customers.jsx
+++ frontend/src/pages/Customers.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
-
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Page,
+  Selection,
+  Inject,
+  Edit,
+  Toolbar,
+  Sort,
+  Filter,
+  Search,
+} from '@syncfusion/ej2-react-grids'
+import { customersData, customersGrid } from '../data/dummy'
+import { Header } from '../components'
 const Customers = () => {
   return (
-    <div>Customers</div>
+    <div className="m-2 md:m-10 mt-20 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Customers" />
+      <GridComponent
+        id="gridcomp"
+        dataSource={customersData}
+        allowPaging
+        allowSorting
+        width="auto"
+        toolbar={['Search', 'Delete']}
+        editSettings={{ allowDeleting: true, allowEditing: true }}
+      >
+        <ColumnsDirective>
+          {customersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject
+          services={[Page, Toolbar, Edit, Selection, Sort, Filter, Search]}
+        />
+      </GridComponent>
+    </div>
   )
 }
 
--- frontend/src/pages/Employees.jsx
+++ frontend/src/pages/Employees.jsx
@@ -1,8 +1,68 @@
 import React from 'react'
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Page,
+  Search,
+  Inject,
+  Toolbar,
+  Edit,
+  ExcelExport,
+  PdfExport,
+} from '@syncfusion/ej2-react-grids'
+import { employeesData, contextMenuItems, employeesGrid } from '../data/dummy'
+import { Header } from '../components'
 
-const Employees = () => {
+const Employees = ({ color }) => {
+  let grid
+  const handlePdfExcel = (args) => {
+    if (grid && args.item.id === 'gridcomp_pdfexport') {
+      grid.showSpinner()
+      grid.pdfExport()
+    } else if (grid && args.item.id === 'gridcomp_excelexport') {
+      grid.showSpinner()
+      grid.excelExport()
+    }
+  }
+  const pdfExportComplete = () => {
+    if (grid) {
+      grid.hideSpinner()
+    }
+  }
+  const excelExportComplete = () => {
+    if (grid) {
+      grid.hideSpinner()
+    }
+  }
+
   return (
-    <div>Employees</div>
+    <div className="m-2 md:m-10 mt-20 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Employees" />
+      <GridComponent
+        id="gridcomp"
+        dataSource={employeesData}
+        allowPaging
+        allowSorting
+        toolbar={['Search', 'PdfExport', 'ExcelExport']}
+        width="auto"
+        allowExcelExport
+        allowPdfExport
+        toolbarClick={handlePdfExcel}
+        pdfExportComplete={pdfExportComplete}
+        excelExportComplete={excelExportComplete}
+        ref={(g) => (grid = g)}
+      >
+        <ColumnsDirective>
+          {employeesGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject
+          services={[Page, Search, Toolbar, Edit, ExcelExport, PdfExport]}
+        />
+      </GridComponent>
+    </div>
   )
 }
 
