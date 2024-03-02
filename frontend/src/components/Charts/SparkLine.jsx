0e3fa263ebc34793709f10b75f62bd29584d4015
FidaHussain87 <36170696+FidaHussain87@users.noreply.github.com>
2024-03-02 01:50:11 +0100

    disable eslint for SparkLine

--- frontend/src/components/Charts/SparkLine.jsx
+++ frontend/src/components/Charts/SparkLine.jsx
@@ -20,6 +20,7 @@ const SparkLine = ({ id, height, width, color, data, type, currentColor }) => {
       type={type}
       tooltipSettings={{
         visible: true,
+        // eslint-disable-next-line no-template-curly-in-string
         format: '${xval} : data ${yval}',
         trackLineSettings: {
           visible: true,
